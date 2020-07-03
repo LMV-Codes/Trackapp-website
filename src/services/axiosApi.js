606ced5f2b52ba0aaa8762b24361988c30a9b6ad
LMV-Codes <vazquez.leo.m@gmail.com>
2020-07-03 11:35:38 -0300

    AxiosApi Change baseURL

--- src/services/axiosApi.js
+++ src/services/axiosApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "http://127.0.0.1:8000/";
+const baseUrl = "https://trackapp-api.herokuapp.com/";
 axios.defaults.xsrfCookieName = "csrftoken";
 axios.defaults.xsrfHeaderName = "X-CSRFTOKEN";
 
